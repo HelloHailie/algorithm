050daf97433cf9bb6d9e2285d35bf2276bb5a55e
HelloHailie <alsltjsals@gmail.com>
2022-06-24 23:37:52 +0900

    0624

--- BOJ/10952_A+B - 5/index.js
+++ BOJ/10952_A+B - 5/index.js
@@ -1,6 +1,13 @@
-const input = require("fs").readFileSync("test.txt").toString().split("\n");
+const input = require("fs")
+  .readFileSync("test.txt")
+  .toString()
+  .trim()
+  .split("\n");
 // console.log(input);
 // console.log(input.length);
+
+// let v = input[0].split(" ");
+// console.log(v[1]);
 
 // console.log(Number(input[0][0]) + Number(input[0][2]));
 // console.log(Number(input[1][0]) + Number(input[1][2]));
@@ -10,7 +17,10 @@ const input = require("fs").readFileSync("test.txt").toString().split("\n");
 // console.log(Number(input[5][0]) + Number(input[5][2]));
 
 for (let i = 0; i < input.length - 1; i++) {
-  console.log(Number(input[i][0]) + Number(input[i][2]));
+  let number = input[i].split(" ").map((e) => e * 1);
+  console.log(number[0] + number[1]);
 }
 
-// if(Number(input[i][0]) === 0 && Number(input[i][2]) === 0) return break;
+// for (let i = 0; i < input.length - 1; i++) {
+//   console.log(Number(input[i][0]) + Number(input[i][2]));
+// }
