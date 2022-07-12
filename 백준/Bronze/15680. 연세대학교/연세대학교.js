778f15ca4f53118d7ff0c43e39278efd11d444d3
HelloHailie <103437860+HelloHailie@users.noreply.github.com>
2022-07-12 23:07:35 +0900

    [Bronze IV] Title: 연세대학교, Time: 112 ms, Memory: 9308 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const input = Number(fs.readFileSync("/dev/stdin").toString().trim());
+
+if (input === 0) {
+  console.log("YONSEI");
+} else if (input === 1) {
+  console.log("Leading the Way to the Future");
+}
