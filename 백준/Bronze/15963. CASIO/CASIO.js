0a793d37d1f3759b76cead6ae4ef3732ece85333
HelloHailie <103437860+HelloHailie@users.noreply.github.com>
2022-07-13 21:59:03 +0900

    [Bronze IV] Title: CASIO, Time: 124 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+if (input[0] === input[1]) {
+  console.log(1);
+} else {
+  console.log(0);
+}
