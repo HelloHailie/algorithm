3b1727c8f1065c035affae1a3e31fc72813ee1ed
HelloHailie <103437860+HelloHailie@users.noreply.github.com>
2022-08-26 09:40:39 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 136 ms, Memory: 9664 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString();
+
+let num = Number(input);
+const r = 2 * num - 1;
+
+for (let i = 1; i <= num; i++) {
+  console.log(" ".repeat(num - i) + "*".repeat(2 * i - 1));
+}
+
+for (let i = num - 1; i > 0; i--) {
+  console.log(" ".repeat(num - i) + "*".repeat(2 * i - 1));
+}
