b2bdf39a6118bd29e710e1c5f93ef3a8f869d053
HelloHailie <alsltjsals@gmail.com>
2022-12-03 23:45:37 +0900

    [level 1] Title: 모의고사, Time: 2.90 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -1,4 +1,35 @@
 function solution(answers) {
-    var answer = [];
+    let answer = [];
+    let oneResult = 0;
+    let twoResult = 0;
+    let threeResult = 0;
+    const one = [1, 2, 3, 4, 5]
+    const two = [2, 1, 2, 3, 2, 4, 2, 5]
+    const three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    
+    for(let i = 0; i < answers.length; i++){
+        if(answers[i] === one[i%5]) oneResult++;
+        if(answers[i] === two[i%8]) twoResult++;
+        if(answers[i] === three[i%10]) threeResult++;
+    }
+    let max = Math.max(oneResult,twoResult,threeResult)
+    
+    if(max === oneResult) answer.push(1);
+    if(max === twoResult) answer.push(2);
+    if(max === threeResult) answer.push(3);
+    
     return answer;
+    // return returnResultFuntion(oneResult,twoResult,threeResult);
 }
+
+// function returnResultFuntion (a,b,c) {
+//     let result = []
+//     if(a > b && a > c) result = [1];
+//     if(b > a && b > c) result = [2];
+//     if(c > a && c > b) result = [3];
+//     if(a === b && a > c) result = [1,2];
+//     if(b === c && b > a) result = [2,3];
+//     if(c === a && c > b) result = [1,3];
+//     if(a === b && b === c && c === a) result = [1,2,3]
+//     return result;
+// }
