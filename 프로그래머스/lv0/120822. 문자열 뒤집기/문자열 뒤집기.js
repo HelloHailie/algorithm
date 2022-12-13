8cd576b36c2d396248ae50d425cfbc29dba4e885
HelloHailie <alsltjsals@gmail.com>
2022-12-13 23:38:57 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -0,0 +1,8 @@
+function solution(my_string) {
+    var answer = '';
+    const lengthNum = my_string.length;
+    for(let i = lengthNum-1; i >= 0; i--){
+        answer += my_string[i]
+    }
+    return answer;
+}
