282d067d99f34971e7163c75e3080a62440a502a
HelloHailie <alsltjsals@gmail.com>
2023-03-19 22:48:44 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    let arr = [];
+    for(let i = 1; i <= Math.sqrt(n); i++){
+        if(n % i === 0){
+            arr.push(i);
+            if(n / i !== i) {
+                arr.push(n/i);
+            }
+        }
+    }
+    return arr.length;
+}
