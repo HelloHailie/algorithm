4b2d929ac8f45aace274f0e11e29703fbf32e1a4
HelloHailie <alsltjsals@gmail.com>
2024-06-05 21:06:52 +0900

    [level 0] Title: 코드 처리하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -0,0 +1,21 @@
+function solution(code) {
+    var answer = '';
+    let mode = 0
+    for(let i = 0; i < code.length; i++) {
+        if(code[i] === '1') {
+            mode = 1 - mode;
+        } else {
+            if(mode === 0 && i % 2 === 0) {
+                answer += code[i]
+            } else if (mode === 1 && i % 2) {
+                answer += code[i]
+            }
+        }
+    }
+    
+    if(answer.length === 0) {
+        return 'EMPTY'
+    } else {
+        return answer;
+    }
+}
